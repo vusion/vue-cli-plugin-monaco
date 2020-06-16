04ea7edb2839dee660bac255e9364e958760e418
Forrest <rainforest92@126.com>
2020-06-16 15:46:02 +0800

    :bug: Fix pluginOptions undefined

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
 
 module.exports = function (api, vueConfig) {
     api.chainWebpack((config) => {
-        config.plugin('monaco-editor').use(MonacoWebpackPlugin, [vueConfig.pluginOptions.monaco]);
+        config.plugin('monaco-editor').use(MonacoWebpackPlugin, [vueConfig.pluginOptions && vueConfig.pluginOptions.monaco]);
     });
 };
